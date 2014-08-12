4e3ef14b1c66ee5828f4564706fcea29c187f117
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-12 21:12:45 +0200

    + create_chart_export_gif.pl

---
+++ Excel/create_chart_export_gif.pl
@@ -0,0 +1,32 @@
+use warnings;
+use strict;
+
+use Win32::Ole;
+use Win32::OLE::Const 'Microsoft.Excel';
+
+use Cwd qw(getcwd);
+use File::Spec;
+
+my $excel = CreateObject Win32::OLE 'Excel.Application' or die;
+$excel->{'Visible'} = 1;
+
+my $workbook = $excel -> workbooks -> add(1);
+my $sheet    = $workbook -> sheets(1);
+my $shape    = $sheet    -> shapes -> addChart;
+my $chart    = $shape    -> chart;
+
+$chart -> {chartType} = xlXYScatterSmoothNoMarkers;
+
+$sheet -> cells(1,1) -> {value} = "x";
+$sheet -> cells(1,2) -> {value} = "sin(x/10)";
+
+for my $row (2..100) {
+  $sheet -> cells($row, 1) ->{value} =     $row / 10 ;
+  $sheet -> cells($row, 2) ->{value} = sin($row / 10);
+}
+
+$chart -> setSourceData($sheet->range($sheet->cells(1,2), $sheet->cells(100,2)));
+
+$chart -> Export(File::Spec->canonpath(getcwd) . "\\sin.gif");
+
+$workbook -> {saved} = 1;
