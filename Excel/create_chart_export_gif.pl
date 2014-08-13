46454588b5520d57660530a30fc59a2397c66829
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-13 09:26:57 +0200

    Use XValues to annotate x axis

--- Excel/create_chart_export_gif.pl
+++ Excel/create_chart_export_gif.pl
@@ -18,14 +18,19 @@ my $chart    = $shape    -> chart;
 $chart -> {chartType} = xlXYScatterSmoothNoMarkers;
 
 $sheet -> cells(1,1) -> {value} = "x";
-$sheet -> cells(1,2) -> {value} = "sin(x/10)";
+$sheet -> cells(1,2) -> {value} = "sin(x)";
 
-for my $row (2..100) {
-  $sheet -> cells($row, 1) ->{value} =     $row / 10 ;
-  $sheet -> cells($row, 2) ->{value} = sin($row / 10);
+my $row = 1;
+for my $x (map {$_ / 10} (0..100)) {
+  $row ++;
+  $sheet -> cells($row, 1) ->{value} =     $x;
+  $sheet -> cells($row, 2) ->{value} = sin($x);
 }
 
 $chart -> setSourceData($sheet->range($sheet->cells(1,2), $sheet->cells(100,2)));
+
+# $chart -> SeriesCollection(1) -> {XValues} = '=Sheet1!$A$2:$A$102';
+$chart -> SeriesCollection(1) -> {XValues} = $sheet -> Range($sheet->cells(2,1), $sheet->cells(2, $row));
 
 $chart -> Export(File::Spec->canonpath(getcwd) . "\\sin.gif");  # Export must not be spelled «export»!
 
